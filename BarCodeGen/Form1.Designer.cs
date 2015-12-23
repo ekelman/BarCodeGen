891f3657cace2368784a138fe428fdbdba8cc8e6
Kelman, Yevgeniy <Yevgeniy.Kelman@capitalone.com>
2015-12-22 22:12:46 -0600

    Signed-off-by: Kelman, Yevgeniy <Yevgeniy.Kelman@capitalone.com>

---
+++ BarCodeGen/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BarCodeGen
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnBarcodeGen = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.SuspendLayout();
+            // 
+            // btnBarcodeGen
+            // 
+            this.btnBarcodeGen.Location = new System.Drawing.Point(77, 105);
+            this.btnBarcodeGen.Name = "btnBarcodeGen";
+            this.btnBarcodeGen.Size = new System.Drawing.Size(113, 47);
+            this.btnBarcodeGen.TabIndex = 0;
+            this.btnBarcodeGen.Text = "Generate Barcode";
+            this.btnBarcodeGen.UseVisualStyleBackColor = true;
+            this.btnBarcodeGen.Click += new System.EventHandler(this.btnBarcodeGen_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnBarcodeGen);
+            this.Name = "Main";
+            this.Text = "Barcode Generator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBarcodeGen;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+    }
+}
+
---
+++ BarCodeGen/Form1.cs
@@ -0,0 +1,99 @@
+﻿using NBarCodes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Printing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarCodeGen
+{
+    public partial class Main : Form
+    {
+        public static string path = Environment.CurrentDirectory;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBarcodeGen_Click(object sender, EventArgs e)
+        {
+            GenBarcode();
+            AppendTextToBarrcode();
+            PrintImage();
+        }
+
+        private static void AppendTextToBarrcode()
+        {
+            var bmp = Bitmap.FromFile(path + @"\barcode.jpg");
+            var newImage = new Bitmap(bmp.Width + 300, bmp.Height);
+            
+            var gr = Graphics.FromImage(newImage);
+            gr.SmoothingMode = SmoothingMode.AntiAlias;
+            gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            gr.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            gr.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
+            gr.DrawImageUnscaled(bmp, 0, 0);
+            gr.DrawString("this is the added text1", new Font("Tahoma", 10), Brushes.Black, new RectangleF(bmp.Width, 0, bmp.Width + 300, 50));
+            gr.DrawString("this is the added text2", new Font("Tahoma", 10), Brushes.Black, new RectangleF(bmp.Width, 15, bmp.Width + 300, 50));
+            gr.DrawString("this is the added text3", new Font("Tahoma", 10), Brushes.Black, new RectangleF(bmp.Width, 30, bmp.Width + 300, 50));
+
+            newImage.Save(path + @"\barcodeandtext.jpg");
+            newImage.Dispose();
+            bmp.Dispose();
+        }
+
+        private static void GenBarcode()
+        {
+            var number = "123456";
+
+            string[] jpegList = Directory.GetFiles(path, "*.jpg");
+            foreach (string f in jpegList)
+            {
+                File.Delete(f);
+            }
+
+            BarCodeSettings settings = new BarCodeSettings();
+            settings.Type = BarCodeType.Code128;
+            settings.Data = number.ToString();
+            settings.BarHeight = 20;
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+            Bitmap b = new Bitmap(generator.GenerateImage());
+            b.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            b.Save(path + @"\barcode.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            b.Dispose();
+        }
+
+        private void PrintImage()
+        {
+            PrintDocument pd = new PrintDocument();
+
+            pd.DefaultPageSettings.Margins = new Margins(0, 0, 0, 0);
+            pd.OriginAtMargins = false;
+            pd.DefaultPageSettings.Landscape = true;
+
+            pd.PrintPage += new PrintPageEventHandler(pd_PrintPage);
+
+
+            printPreviewDialog1.Document = pd;
+            printPreviewDialog1.ShowDialog();
+
+            //pd.Print();
+        }
+
+        void pd_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            var image = Bitmap.FromFile(path + @"\barcodeandtext.jpg");
+            e.Graphics.DrawImage(image, 0, 0, image.Width, image.Height);
+            image.Dispose();
+        }
+    }
+}
